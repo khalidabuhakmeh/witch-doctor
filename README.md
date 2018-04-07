8ef3a0017db41632e46de9d5f6fdf133c656d1aa
Khalid Abuhakmeh <khalid@aquabirdconsulting.com>
2018-04-07 15:15:26 -0400

    working witch doctor for now

--- README.md
+++ README.md
@@ -1,3 +1,48 @@
-# witch-doctor
+# Witch Doctor
 
-a command-line tool for pulling documentation from multiple git repositories into one project for static site generation.
+![witch doctor](witch-doctor.png)
+
+a command-line tool for pulling documentation from several GitHub repositories into one project for static site generation.
+
+## Philosophy
+
+Documentation thrives when two rules are followed:
+
+1. Authoring documentation occurs closest to the code it represents (hopefully in the same repo).
+2. Reading documentation occurs in one common place where it can enforce a habit.
+
+## Getting Started
+
+First you will need to create a `witch-doctor.json` file in the directory you wish to merge documentation into. This file will look like:
+
+```json
+{
+    "projects": [
+        {
+            "name" : "<name>",
+            "source": "<source directory>",
+            "target": "<target directory>",
+            "repository" : "<github repository name>",
+            "branch" : "<repository name>",
+            "token" : "<OAuth token> (optional for private repos)"
+        }
+    ]
+}
+```
+
+Next install the `witch-doctor` npm package.
+
+```console
+> npm install witch-doctor
+> npm install -g witch-doctor
+```
+
+Finally, as part of a build, run `witch-doctor`.
+
+## What Happens?
+
+Witch Doctor will clone all the repositories in your `witch-doctor.json` file and begin to copy the files from `source` to `target`. Once, all projects are processed, you can run the build script for your static site generator of choice: Jekyll, Hugo, etc.
+
+## Credit
+
+- [Shaman by Gan Khoon Lay (Noun Project)](https://thenounproject.com/term/shaman/659848/)
--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 #!/usr/bin/env node
-var git = require('nodegit');
-var co = require('co');
-var prompt = require('co-prompt');
+var NodeGit = require('nodegit');
 var program = require('commander');
 var fs = require('fs');
+var path = require('path');
+var copy = require('recursive-copy');
+var rimraf = require('rimraf');
+
+var tempPath = path.join(__dirname, "tmp");
+var localPath = path.join(__dirname);
 
 program
     .arguments('<file>')
-    .option('-u, --username <username>', 'The user to authenticate as')
-    .option('-p, --password <password>', 'The user\'s password')
-    .action(function(file) {
-        console.log('user: %s pass: %s file: %s',
-        program.username, program.password, file);
+    .action(async function (file) {
+        console.log('file: %s', file);
+        var contents = fs.readFileSync(file);
+        var configuration = JSON.parse(contents);
+
+        await Promise
+            .all(configuration.projects.map(async p => {
+                console.log('processing "%s" [%s]', p.name, p.repository);
+                await processProject(p)
+            }))
+            .catch((err) => console.log(err));
+
+        if (fs.existsSync(tempPath)) {
+            rimraf(tempPath, () => console.log('cleaning up all of temp'));
+        };
     })
     .parse(process.argv);
+
+async function processProject(project) {
+
+    var cloneUrl = `https://github.com/${project.repository}.git`;
+    var cloneOptions = {
+        checkoutBranch: project.branch
+    };
+    cloneOptions.fetchOpts = {
+        callbacks: {
+            certificateCheck: function () { return 1; },
+        }
+    };
+
+    if (project.token) {
+        cloneURL = `https://${project.token}:x-oauth-basic@github.com/${project.repository}`;
+        cloneOptions.fetchOpts.callbacks.credentials =
+            function () {
+                return NodeGit.Cred.userpassPlaintextNew(project.token, "x-oauth-basic");
+            };
+    }
+
+    var repoPath = path.join(tempPath, project.name);
+    var source = path.join(repoPath, project.source);
+    var destination = path.join(__dirname, project.target);
+
+    var errorAndAttemptOpen = function (error) {
+        console.log(error);
+        return NodeGit.Repository.open(repoPath);
+    };
+
+    var cloneRepository = NodeGit.Clone(cloneUrl, repoPath, cloneOptions);
+
+    await cloneRepository
+        .catch(errorAndAttemptOpen)
+        .then(function (repository) {
+            console.log('cloned and copying files from %s to %s', source, destination);
+            copy(source, destination, { overwrite : true })
+                .then(function (results) {
+                    console.info('Copied ' + results.length + ' files');
+                })
+                .catch(function (error) {
+                    console.error('Copy failed: ' + error);
+                });
+        });
+}
